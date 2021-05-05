92eb92d901dc9943abb302254d6d3ba5ef1964c2
reza <reza001391@gmail.com>
2021-05-05 22:03:20 +0430

    add discount box

---
+++ mentor/Site/js/custom.js
@@ -0,0 +1,7 @@
+
+// discount close btn
+var close = document.querySelector(".closeBtn");
+close.addEventListener("click",off);
+function off(){
+    document.querySelector(".discount").style.visibility = "hidden";
+}
