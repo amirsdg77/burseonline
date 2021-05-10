a74d0b5a9559fa97a8da65294b20788282391a85
reza <reza001391@gmail.com>
2021-05-10 17:20:44 +0430

    Complete the first step of the index.html page

--- mentor/Site/js/custom.js
+++ mentor/Site/js/custom.js
@@ -1,7 +1,7 @@
 
 // discount close btn
 var close = document.querySelector(".closeBtn");
-close.addEventListener("click",off);
-function off(){
+close.addEventListener("click", off);
+function off() {
     document.querySelector(".discount").style.visibility = "hidden";
 }
