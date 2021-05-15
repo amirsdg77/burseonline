968d8f5178b9bf0818ae9f8ff9d2e37580373dde
reza <reza001391@gmail.com>
2021-05-15 11:30:25 +0430

    add the login page and complete half of it

--- mentor/Site/js/custom.js
+++ mentor/Site/js/custom.js
@@ -5,3 +5,17 @@ close.addEventListener("click", off);
 function off() {
     document.querySelector(".discount").style.visibility = "hidden";
 }
+
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+    container.classList.add('right-panel-active');
+});
+
+signInButton.addEventListener('click', () => {
+    container.classList.remove('right-panel-active');
+});
+
+
